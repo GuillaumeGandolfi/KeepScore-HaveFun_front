300e741c1d7a92745a1fc79c9a83f90dc3793075
Abdelaziz Bouhayoufi <abdelaziz.bouhayoufi@gmail.com>
2023-04-13 10:07:51 +0200

    loading page modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./styles/App.css";
-import Loader from "./components/Loader/Loader";
+import Loading from "./components/Loading/Loading";
 import ConnectionForm from "./components/ConnectionForm/ConnectionForm";
 
 function App() {
   return (
     <div className="App">
+      <Loading />
     </div>
   );
 }
