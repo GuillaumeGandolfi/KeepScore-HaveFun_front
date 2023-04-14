f0d3fd84d92a422c31fdee0dd842b57e0f6e9175
Abdelaziz Bouhayoufi <abdelaziz.bouhayoufi@gmail.com>
2023-04-14 10:39:30 +0200

    cleaning

--- src/reducer/appReducer.js
+++ src/reducer/appReducer.js
@@ -1,23 +1,23 @@
-import { CHANGE_EMAIL_FIELD, CHANGE_PASSWORD_FIELD } from "../store/actions/actions";
-
+import {
+  CHANGE_EMAIL_FIELD,
+  CHANGE_PASSWORD_FIELD,
+} from "../store/actions/actions";
 
 const initialState = {
-    isLogged : false,
-    emailField:'',
-    passwordField: ''
+  isLogged: false,
+  emailField: "",
+  passwordField: "",
+};
 
-}
-
-const reducer = (state = initialState, action={})=>{
-
-    switch(action.type){
-        case CHANGE_EMAIL_FIELD:
-            return  {...state, emailField: action.input }
-            case CHANGE_PASSWORD_FIELD:
-            return  {...state, passwordField: action.input }
-        default:
-            return state
-    }
-}
+const reducer = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case CHANGE_EMAIL_FIELD:
+      return { ...state, emailField: action.input };
+    case CHANGE_PASSWORD_FIELD:
+      return { ...state, passwordField: action.input };
+    default:
+      return state;
+  }
+};
 
 export default reducer;
