408e06f9702598d03038981d2725e5c04790228c
nefzisondes94 <nefzi.sondes@outlook.fr>
2023-04-28 12:30:37 +0200

    refactor: update shops reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,19 +1,19 @@
-import { configureStore } from '@reduxjs/toolkit'
+import {
+  configureStore
+} from '@reduxjs/toolkit'
 import authMiddleware from '../middlewares/authenticationMiddleware'
 import fetchDataMiddleware from '../middlewares/fetchDataMiddleware'
-import { applyMiddleware } from '@reduxjs/toolkit'
+import {
+  applyMiddleware
+} from '@reduxjs/toolkit'
 import rootReducer from '../reducer/rootReducer'
 import signupMiddleware from '../middlewares/signupMiddleware'
-
-
-
-
+import fetchShopsDataMiddleware from '../middlewares/fetchShopDataMiddleware'
 
 
 
 export default configureStore({
-   reducer : rootReducer,
-   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authMiddleware, fetchDataMiddleware, signupMiddleware),
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authMiddleware, fetchDataMiddleware, signupMiddleware, fetchShopsDataMiddleware),
   // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authMiddleware),
 })
-
