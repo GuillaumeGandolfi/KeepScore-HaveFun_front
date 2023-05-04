0c3880b06369601f98a129102260d9edb195b495
AABouh <abdelaziz.bouhayoufi@gmail.com>
2023-05-04 14:51:13 +0200

    navlink active added

--- src/axios/axiosPrivate.js
+++ src/axios/axiosPrivate.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 import { memoizedRefreshToken } from "./refreshToken"; 
 
-axios.defaults.baseURL = "http://localhost:3000/";
+axios.defaults.baseURL = "https://avid-addition-production.up.railway.app";
 
 axios.interceptors.request.use(
   async (config) => {
