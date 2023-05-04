af451238c5693dffb1afe5c9b7f3df1b7f54bfb4
Tiuko <108450912+Tiuko@users.noreply.github.com>
2023-05-04 09:03:41 +0200

    correction of budgetpage branch with an error on Shoppage.jsx

--- src/components/pages/Shoppage/Shoppage.jsx
+++ src/components/pages/Shoppage/Shoppage.jsx
@@ -1,32 +1,27 @@
-import { Grid, Container,  } from '@mui/material';
+import { Grid, Container  } from '@mui/material';
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
+import { fetchShopsData } from '../../../store/actions/fetchShopsDataActions';
 import { ProductViews } from '../../ProductView/ProductView';
 
-import { getProducts } from '../../../reducer/shopReducer.js'
+import './shopsPage.css'
 
-import './shoppage.css'
-
-
-
-function shoppingPage() {
+function ShoppingPage() {
     const dispatch = useDispatch()
-    const { products, loading } = useSelector((state) => state.shops)
-
+    const {products, loading} = useSelector(state => state.shops)
     useEffect(() => {
-      dispatch(getProducts())
-    }, [])
- 
+        dispatch(fetchShopsData());
+    }, []);
+
     if (loading) return <p>Loading...</p>
 
-     return (
+    return (
         <Container sx={{ py: 8 }} maxWidth="md">
-         <Grid container spacing={4}>
-            {products.map(item => <ProductViews key={item.id} />)}
+            <Grid container spacing={4}>
+                {products.map(item => <ProductViews key={item.id} />)}
             </Grid>
         </Container>
     );
 }
 
-
-export default shoppingPage;
+export default ShoppingPage;
