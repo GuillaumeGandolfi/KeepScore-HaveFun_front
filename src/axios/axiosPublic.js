06316ce5b876983c2a0953e5cdd6628d3014114d
AABouh <abdelaziz.bouhayoufi@gmail.com>
2023-05-04 13:09:55 +0200

    database url

--- src/axios/axiosPublic.js
+++ src/axios/axiosPublic.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosPublic = axios.create({
-    baseURL: "https://projet-09-keep-score-and-have-fun-back.vercel.app/",
+    baseURL: "http://localhost:3000",
     headers: {
         "Content-Type": "application/json"
     },
