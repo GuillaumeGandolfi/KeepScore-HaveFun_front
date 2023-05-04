068031f83b7b89983ab667680db773b2c653248a
Tiuko <108450912+Tiuko@users.noreply.github.com>
2023-05-04 12:04:26 +0200

    fix problems on fetchDataMiddleware

--- src/axios/axiosPrivate.js
+++ src/axios/axiosPrivate.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 import { memoizedRefreshToken } from "./refreshToken"; 
 
-axios.defaults.baseURL = "https://projet-09-keep-score-and-have-fun-back.vercel.app/";
+axios.defaults.baseURL = "http://localhost:3000/";
 
 axios.interceptors.request.use(
   async (config) => {
